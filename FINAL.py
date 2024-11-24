c7733b893ebf0ebd0d14edb2a1aff57ab6a78247
Эллина <Arlekino@mail.ru>
2024-11-24 22:01:02 +0300

    Создание класса Restaurant и методов в FINAL.py

--- FINAL.py
+++ FINAL.py
@@ -17,3 +17,30 @@ class Menu:
     def print_menu(self,dish):
         print(dish)
         print(f"Состав блюда: {", ".join(map(str,self.ingredients))}\n")
+
+class Restaurant:
+    def __init__(self,title,menu,client):
+        self.title = title
+        self.menu = menu
+        self.name = client.name
+        self.pay_order = "Ожидание оплаты заказа"
+        self.status_order = "успешно принят"
+
+    def payment_order(self):
+        print(f"Статус оплаты заказа, клиента {self.name}:\n")
+        time.sleep(3)
+        print(f"{self.pay_order}")
+        self.pay_order = "Заказ оплачен"
+        time.sleep(3)
+        print(f"{self.pay_order}")
+        self.pay_order = "Оплата получена"
+        time.sleep(3)
+        print(f"{self.pay_order}")
+    
+    def accept_order(self):
+        print(f"Статус заказа клиента {self.name}:\n")
+        time.sleep(3)
+        print(f"Клиент {self.name}, ваш заказ {self.status_order}")
+        self.status_order = "успешно оформлен"
+        time.sleep(3)
+        print(f"Клиент {self.name}, ваш заказ {self.status_order}")
